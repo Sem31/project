7303a5a265486d0470d729eded8cfec2c1b57297
Sem31 <31kamleshprajapat@gmail.com>
2019-12-07 01:54:02 +0530

    website

--- website/signup/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Signup,Login
-
-admin.site.register(Signup)
-admin.site.register(Login)
--- website/signup/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-	path('registration', views.user_registration, name='user_registration'),
-	path('login', views.user_login, name='user_login'),
-	path('welcome/<str:context>',views.user_welcome,name = "user_welcome")
-]
